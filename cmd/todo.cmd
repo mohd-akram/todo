5566ef5e71066936f1a9e56e6b0261c9d3c335a1
Mohamed Akram <mohd.akram@outlook.com>
2016-01-24 03:20:08 +0400

    Simpler Windows command script

--- cmd/todo.cmd
+++ cmd/todo.cmd
@@ -1,10 +1,11 @@
 @echo off
+setlocal
 
 rem this is where todo.md will be stored if the file doesn't exist locally
 set folder=%USERPROFILE%\OneDrive\Documents
 
 if exist todo.md (
-  "%~dp0\..\bin\todo" %*
+  "%~dp0..\bin\todo" %*
 ) else (
-  setlocal & cd %folder% && "%~dp0\..\bin\todo" %* & endlocal
+  cd %folder% && "%~dp0..\bin\todo" %*
 )
